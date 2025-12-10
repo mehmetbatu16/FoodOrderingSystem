1bb3e09d050cd7eb6e36f7eddbfaecd7a1d6e47e
Mehmet Batu Buladi <mehmetbuladi23@istanbularel.edu.tr>
2025-12-11 02:02:43 +0300

    Added interfaces for Order and Payment systems

--- Main.java
+++ Main.java
@@ -1,7 +1,14 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+interface Orderable {
+    void placeOrder();
+    double calculateTotal();
+}
 
+interface PaymentMethod {
+    void pay(double amount);
+}
 public class Main {
     public static void main(String[] args) {
         System.out.println("System starting...");
